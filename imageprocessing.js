db1f134b93a4b23a13e538d2bcb1942ddfc6961a
Tom Kendall <tomkendall1234@gmail.com>
2022-10-17 15:49:55 +0100

    Split javascript into separate file

---
+++ imageprocessing.js
@@ -0,0 +1,70 @@
+function openCvReady() {
+    cv['onRuntimeInitialized']=()=>{
+      let video = document.getElementById("videoInput");
+      if( /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+        video.width = 600;
+        video.height = 800;
+      } else {
+        video.width = 800;
+        video.height = 600;
+      }
+  
+      navigator.mediaDevices
+        .getUserMedia({ audio: false, video:{facingMode: 'environment'}})
+        .then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+  
+      let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+      let dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+      let cap = new cv.VideoCapture(video);
+  
+      const FPS = 30;
+      function processVideo() {
+        try {
+          let begin = Date.now();
+          // start processing.
+          cap.read(src);
+          cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+          let ksize = new cv.Size(17, 17);
+          cv.GaussianBlur(dst, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+  
+          let color = new cv.Scalar(255, 0, 0, 1);
+          let circles = new cv.Mat;
+          cv.HoughCircles(dst, circles, cv.HOUGH_GRADIENT, 1.2, 100, 150, 30, 50, 400);
+          for (let i = 0; i < circles.cols; ++i) {
+            let x = circles.data32F[i * 3];
+            let y = circles.data32F[i * 3 + 1];
+            let radius = circles.data32F[i * 3 + 2];
+            let center = new cv.Point(x, y);
+              try {
+                cv.circle(src, center, (radius * 1.1), [255, 0, 0, 255], 2);
+                console.log(center)
+                console.log(radius)
+              } catch (err) {
+              }
+          }
+  
+  
+          cv.imshow("canvasOutput", src);
+          
+          // schedule the next one.
+          let delay = 1000 / FPS - (Date.now() - begin);
+          setTimeout(processVideo, delay);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+  
+      // schedule the first frame.
+      setTimeout(processVideo, 0);
+    })
+    .catch(function(err) {
+      console.log("An error occurred! " + err);
+    });
+  
+  
+    }
+  
+  }
+  
